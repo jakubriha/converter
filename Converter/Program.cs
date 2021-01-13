d28cf5de3aad7b864378d6d4c84bc91088bdda55
Jakub Riha <jakubriha@outlook.com>
2021-01-13 19:21:53 +0100

    Add starting code
    Inspired by https://github.com/dotnet/command-line-api/blob/main/samples/HostingPlayground.

---
+++ Converter/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    public class Program
+    {
+        static async Task<int> Main(string[] args) =>
+            await BuildCommandLine()
+                .UseHost(_ => Host.CreateDefaultBuilder(),
+                    host =>
+                    {
+                        host.ConfigureServices((hostContext, services) =>
+                        {
+                            services.AddTransient<RootService>();
+                        });
+                    })
+                .UseDefaults()
+                .Build()
+                .InvokeAsync(args);
+
+        private static CommandLineBuilder BuildCommandLine()
+        {
+            var root = new RootCommand()
+            {
+                new Option<string>("--name")
+                {
+                    IsRequired = true,
+                    Description = "Name"
+                }
+            };
+
+            root.Handler = CommandHandler.Create<ProgramOptions, IHost>(Run);
+
+            return new CommandLineBuilder(root);
+        }
+
+        private static void Run(ProgramOptions programOptions, IHost host)
+        {
+            var srv = host.Services.GetService<RootService>();
+
+            srv.Run(programOptions);
+        }
+    }
+
+    public class RootService
+    {
+        public void Run(ProgramOptions programOptions)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class ProgramOptions
+    {
+        public string Name { get; set; }
+    }
+}
