99fc44896ad230deda0f160fbf930d014a437ea0
Jakub Riha <jakubriha@outlook.com>
2021-01-15 19:44:24 +0100

    Implement FormatSerializer

---
+++ Converter.Tests/FormatSerializerTests.cs
@@ -0,0 +1,29 @@
+using Converter.Models;
+using Converter.Services;
+using FluentAssertions;
+using System.Text;
+using System.Xml.Linq;
+using Xunit;
+
+namespace Converter.Tests
+{
+    public class FormatSerializerTests
+    {
+        [Fact]
+        public void Xml_DocumentIsSerialized_SerializedDocumentReturned()
+        {
+            IFormatSerializer service = new XmlFormatSerializer();
+
+            var expectedDocument = new XDocument(
+                new XElement("Document",
+                    new XElement("Title", "a"),
+                    new XElement("Text", "b")));
+
+            byte[] xmlBytes = service.Serialize(new Document("a", "b"));
+
+            XDocument parsedDocument = XDocument.Parse(Encoding.UTF8.GetString(xmlBytes));
+
+            parsedDocument.Should().BeEquivalentTo(expectedDocument);
+        }
+    }
+}
---
+++ Converter/Services/FormatSerializer.cs
@@ -0,0 +1,25 @@
+﻿using Converter.Models;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Converter.Services
+{
+    internal interface IFormatSerializer
+    {
+        byte[] Serialize(Document document);
+    }
+
+    internal class XmlFormatSerializer : IFormatSerializer
+    {
+        public byte[] Serialize(Document document)
+        {
+            var serializer = new XmlSerializer(typeof(Document));
+
+            using var memoryStream = new MemoryStream();
+
+            serializer.Serialize(memoryStream, document);
+
+            return memoryStream.ToArray();
+        }
+    }
+}
