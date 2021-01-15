ec542c0a09da0443ead98a90a2c016b1771b9f63
Jakub Riha <jakubriha@outlook.com>
2021-01-15 18:54:18 +0100

    Implement FileSystemDataWriter

---
+++ Converter/Services/DataWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Abstractions;
+
+namespace Converter.Services
+{
+    internal interface IDataWriter
+    {
+        void WriteAllBytes(Uri location, byte[] data);
+    }
+
+    internal class FileSystemDataWriter : IDataWriter
+    {
+        private readonly IFileSystem fileSystem;
+
+        public FileSystemDataWriter(IFileSystem fileSystem) =>
+            this.fileSystem = fileSystem;
+
+        public void WriteAllBytes(Uri location, byte[] data) =>
+            fileSystem.File.WriteAllBytes(location.LocalPath, data);
+    }
+}
