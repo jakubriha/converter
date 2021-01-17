35651851044e98a3a5953baf83a8e5f7132b9942
Jakub Riha <jakubriha@outlook.com>
2021-01-17 16:17:40 +0100

    Implement correct data reader selection in ProgramPipeline

---
+++ Converter/Services/ProgramPipeline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Converter.Services
+{
+    internal interface IProgramPipeline
+    {
+        IDataReader SelectCorrectDataReader(Uri location);
+    }
+
+    internal class ProgramPipeline : IProgramPipeline
+    {
+        private readonly IEnumerable<IDataReader> dataReaders;
+
+        public ProgramPipeline(IEnumerable<IDataReader> dataReaders)
+        {
+            if (!dataReaders.Any())
+            {
+                throw new ArgumentOutOfRangeException(nameof(dataReaders), "No data readers provided.");
+            }
+
+            this.dataReaders = dataReaders;
+        }
+
+        public IDataReader SelectCorrectDataReader(Uri location) =>
+            dataReaders.FirstOrDefault(dataReader => dataReader.IsValidLocation(location));
+    }
+}
